de2d930235a14806692f81f20ad8309f5b2b04b5
Diego Vidarte <diegovidarte2022@gmail.com>
2025-11-25 16:39:33 -0500

    calculo_frecuencia

---
+++ shared/psi_sdk/config.py
@@ -0,0 +1,15 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+
+    ctrlx_opcua_url: str = "opc.tcp://192.168.18.32:4840"
+    ctrlx_opcua_user: str | None = None
+    ctrlx_opcua_password: str | None = None
+    ctrlx_opcua_period_s: float = 0.1
+
+    # estos pueden existir pero ya no los usaremos:
+    ctrlx_opcua_node_ids: str = ""
+    ctrlx_opcua_root_path: str | None = None
+
+    class Config:
+        env_file = ".env"
